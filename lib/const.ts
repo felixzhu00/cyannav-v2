2f985db4ee0c0a7d615f6d5732cc3595efc43cb4
Tommy <lin.tommy@outlook.com>
2024-08-19 01:15:22 -0400

    Created the features section on landing page

--- lib/const.ts
+++ lib/const.ts
@@ -1,5 +1,14 @@
 // Import any necessary components
+import React from 'react'
 import dynamic from 'next/dynamic'
+import {
+  Map,
+  LayoutTemplate,
+  MessageCircle,
+  Download,
+  BellRing,
+  Users,
+} from 'lucide-react'
 
 // Dynamically import heavy components
 const Choropleth = dynamic(() => import('../components/templates/Choropleth'))
@@ -168,5 +177,124 @@ export const sampleComments = [
     message: "Let's set up a meeting to discuss further.",
     user: 'cloudwalker',
     time: new Date('2024-08-12T10:50:55'),
+  },
+]
+
+export const features = [
+  {
+    icon: Map,
+    featureTitle: 'Multiple Map Type Support',
+    featureDescription:
+      'Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl. Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl.',
+    comingSoon: '',
+  },
+  {
+    icon: LayoutTemplate,
+    featureTitle: 'Templates',
+    featureDescription:
+      'Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl. Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl.',
+    comingSoon: '',
+  },
+  {
+    icon: MessageCircle,
+    featureTitle: 'Dedicated Chat',
+    featureDescription:
+      'Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl. Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl.',
+    comingSoon: '',
+  },
+  {
+    icon: Download,
+    featureTitle: 'Download Your Creations',
+    featureDescription:
+      'Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl. Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl.',
+    comingSoon: '',
+  },
+  {
+    icon: BellRing,
+    featureTitle: 'Notifications',
+    featureDescription:
+      'Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl. Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl.',
+    comingSoon: '',
+  },
+  {
+    icon: Users,
+    featureTitle: 'Multi-User Editing',
+    featureDescription:
+      'Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl. Lorem ipsum dolor sit amet consectetur. Sit venenatis posuere sapien pretium adipiscing dictum ut sagittis nisl.',
+    comingSoon: 'Coming soon for Pro users!',
+  },
+]
+
+export const pricingModels = [
+  {
+    title: 'Free',
+    monthlyPrice: '$0/month',
+    yearlyPrice: '$0/year',
+    description: 'Forever free!',
+    features: [
+      'Building an creating multiple maps and being able to save it to your file',
+      'Feature 2',
+      'Feature 3',
+    ],
+  },
+  {
+    title: 'Pro',
+    monthlyPrice: '$9.99/month',
+    yearlyPrice: '$99.99/year',
+    description: 'Even more features for our pro users!',
+    features: ['Feature 1', 'Feature 2', 'Feature 3'],
+  },
+]
+
+export const faqData = [
+  {
+    question: 'How to use CyanNav?',
+    answer:
+      'CyanNav is easy to use! Simply include it in your project, and use the provided components to create navigation menus. Customize it as needed using the available props and styles.',
+  },
+  {
+    question: 'Is CyanNav compatible with all browsers?',
+    answer:
+      'Yes, CyanNav is designed to be compatible with all modern browsers, ensuring a consistent experience across different platforms.',
+  },
+  {
+    question: 'Can I customize the appearance of CyanNav?',
+    answer:
+      'Absolutely! CyanNav allows for extensive customization, including color schemes, font sizes, and layout options, to match your project’s design.',
+  },
+  {
+    question: 'Does CyanNav support responsive design?',
+    answer:
+      'Yes, CyanNav is fully responsive and adjusts automatically to different screen sizes, ensuring optimal navigation on both desktop and mobile devices.',
+  },
+  {
+    question: 'How do I integrate CyanNav with my existing project?',
+    answer:
+      'To integrate CyanNav with your project, simply install the package via npm or yarn, import the necessary components, and include them in your layout. Detailed documentation is available to guide you through the process.',
+  },
+  {
+    question: 'Is CyanNav accessible?',
+    answer:
+      'Yes, CyanNav adheres to accessibility standards, including ARIA attributes, ensuring that all users, including those with disabilities, can navigate your site easily.',
+  },
+  {
+    question: 'Can I use CyanNav with other UI libraries?',
+    answer:
+      'Yes, CyanNav is designed to be flexible and can be integrated with most UI libraries and frameworks, allowing you to enhance your existing project without conflict.',
+  },
+  {
+    question: 'Is there support available if I run into issues?',
+    answer:
+      'Yes, our support team is available to help with any issues you may encounter. You can also refer to our extensive documentation and community forums for additional help.',
+  },
+  {
+    question: 'Does CyanNav support multi-level menus?',
+    answer:
+      'Yes, CyanNav supports multi-level menus, making it easy to create complex navigation structures for your site.',
+  },
+  {
+    question: 'Can I add icons to the navigation items?',
+    answer:
+      'Yes, CyanNav allows you to add icons to your navigation items, enhancing the visual appeal and usability of your menus.',
   },
 ]

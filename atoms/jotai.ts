24b9dabd6075029d32f589b2718fd4db86d10c90
felixzhu00 <felix.zhu00@gmail.com>
2024-08-25 04:34:36 -0400

    init jotai states

---
+++ atoms/jotai.ts
@@ -0,0 +1,28 @@
+import { atom } from 'jotai'
+import { MapSchemaDecodedType } from '@/lib/types'
+
+// Init as None
+export const selectedEditOptionAtom = atom('')
+
+export const currLayerAtom = atom('')
+
+// Will have callback to API for every change
+// Init from API GET request
+
+// Define the initial state or default values if needed
+const EMPTY_MAP_DATA: MapSchemaDecodedType = {
+  title: '',
+  owner: '',
+  mapType: '',
+  isPublished: false,
+  geojson: undefined,
+  thumbnail: undefined,
+  likes: [],
+  messages: [],
+  sharedUsers: [],
+  dateCreated: new Date(),
+  forkedFrom: [],
+}
+
+// Atom for holding the map data, initially set to EMPTY_MAP_DATA
+export const mapAtom = atom(EMPTY_MAP_DATA)
